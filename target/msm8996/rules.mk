Android4Lumia/bootloader_msm8227

[EFIDROID] allow to externally set MEMBASE and MEMSIZE

target/msm8996/rules.mk
@@ -5,8 +5,8 @@ INCLUDES += -I$(LK_TOP_DIR)/dev/gcdb/display -I$(LK_TOP_DIR)/dev/gcdb/display/in
 
 PLATFORM := msm8996
 
-MEMBASE := 0x8F000000 # SDRAM
-MEMSIZE := 0x00200000 # 2MB
+MEMBASE ?= 0x8F000000 # SDRAM
+MEMSIZE ?= 0x00200000 # 2MB
 
 BASE_ADDR    := 0x0000000
 

TeamRegular/bootloader_lk

target/msm8996/rules.mk
@@ -8,8 +8,8 @@ endif
 
 PLATFORM := msm8996
 
-MEMBASE := 0x91600000 # SDRAM
-MEMSIZE := 0x00400000 # 4MB
+MEMBASE ?= 0x91600000 # SDRAM
+MEMSIZE ?= 0x00400000 # 4MB
 
 BASE_ADDR    := 0x0000000
 